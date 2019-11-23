4dd5376728ec6ecbba9e9d926f2164a54e7971bc
Hy-Vong <hyvong.georges@gmail.com>
2019-11-22 19:01:19 -0500

    Import des assets du towerdefense

---
+++ JeuGameJam/Assets/Scripts/EnnemyController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EnnemyController : MonoBehaviour
+{
+    public float Speed = 1f;
+
+    private Path m_Path;
+    private Tuile m_CurrentTarget;
+
+    private AudioSource m_AudioSource;
+    [SerializeField] private List<AudioClip> m_SpawnSounds;
+
+    public Path Path
+    {
+        set { m_Path = value; }
+    }
+
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        m_CurrentTarget = m_Path?.GetNextTuile(m_CurrentTarget);
+
+        if (m_Path == null || m_CurrentTarget == null)
+        {
+            Debug.LogWarning("Ennemy has no possible path.");
+            gameObject.SetActive(false);
+            return;
+        }
+
+        //Teleporte sur le point de départ
+        transform.position = m_CurrentTarget.transform.position;
+
+        m_AudioSource.pitch = Random.Range(0.9f, 1.1f);
+        m_AudioSource.PlayOneShot(m_SpawnSounds[Random.Range(0, m_SpawnSounds.Count)]);
+    }
+
+    void Update()
+    {
+        float t_DistanceAFaire = Speed * Time.deltaTime;
+
+        Vector3 t_StartPoint = transform.position;
+        Vector3 T_MoveToNextCheckpoint = m_CurrentTarget.transform.position - t_StartPoint;
+
+        while (t_DistanceAFaire * t_DistanceAFaire >= T_MoveToNextCheckpoint.sqrMagnitude && m_CurrentTarget != null)
+        {
+            t_DistanceAFaire -= T_MoveToNextCheckpoint.magnitude;
+            t_StartPoint = m_CurrentTarget.transform.position;
+            m_CurrentTarget = m_Path.GetNextTuile(m_CurrentTarget);
+
+            if (m_CurrentTarget == null)
+                continue;
+
+            T_MoveToNextCheckpoint = m_CurrentTarget.transform.position - t_StartPoint;
+        }
+
+        //Je suis au bout du Path
+        if (m_CurrentTarget == null)
+        {
+            transform.position = t_StartPoint;
+            TargetReached();
+        }
+        else
+        {
+            transform.position = t_StartPoint + (m_CurrentTarget.transform.position - t_StartPoint).normalized * t_DistanceAFaire;
+        }
+    }
+
+    private void TargetReached()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (m_Path == null || m_Path.Checkpoints.Count < 2)
+            return;
+
+        Gizmos.color = Color.cyan;
+
+        for (int i = 0; i < m_Path.Checkpoints.Count - 1; i++)
+        {
+            Gizmos.DrawLine(m_Path.Checkpoints[i].transform.position, m_Path.Checkpoints[i + 1].transform.position);
+        }
+    }
+}
---
+++ JeuGameJam/Assets/Scripts/EnnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemySpawner : MonoBehaviour
+{
+    public GameObject PREFAB_Ennemy;
+    public Tuile TargetTuile;
+
+
+    private void Awake()
+    {
+        if(PREFAB_Ennemy.GetComponent<EnnemyController>() == null)
+        {
+            Debug.LogError("EnnemySpawner is spawning an object without an EnnemyController.");
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 2f, 2f);
+    }
+
+    private void Spawn()
+    {
+        GameObject t_Ennemy = Instantiate(PREFAB_Ennemy);
+        EnnemyController t_EnnemyController = t_Ennemy.GetComponent<EnnemyController>();
+
+        //TODO Pour le moment, on a juste une grille, donc un Pathfinder, donc ça marche.
+        Pathfinder t_Pf = FindObjectOfType<Pathfinder>();
+        t_EnnemyController.Path = t_Pf.GetPath(this.transform, TargetTuile, false);
+    }
+}
