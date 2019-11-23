896fcf1da01a3ee1dbb059272d3ca611e7374b7d
Félix Brunelle <felixbrunelle@hotmail.fr>
2019-11-23 16:13:37 -0500

    Hero collision, ennemy collision

---
+++ JeuGameJam/Assets/Scripts/HpMonstre.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpMonstre : MonoBehaviour
+{
+    public int Hp = 1;
+    public Animator animator;
+
+    private bool Died;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Died = false;
+    }
+
+    private void Update()
+    {
+        if (Hp == 0 && !Died)
+        {
+            Died = true;
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("IsDead");
+        
+    }
+
+    private void KillMonster()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+}
