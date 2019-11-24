90b61d776f3770ff6b63706c3a0edff3851d4f7e
Félix Brunelle <felixbrunelle@hotmail.fr>
2019-11-23 22:36:50 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ JeuGameJam/Assets/Scripts/ButtonBack.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBack : MonoBehaviour
+{
+    public void LoadDefault()
+    {
+        SceneManager.LoadScene("InterfaceDefault");
+    }
+}
---
+++ JeuGameJam/Assets/Scripts/ButtonOption.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOption : MonoBehaviour
+{
+    public void LoadOption()
+    {
+        SceneManager.LoadScene("InterfaceOption");
+    }
+}
